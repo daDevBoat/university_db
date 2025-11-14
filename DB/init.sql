6cdd1e4445bdf4fce7fb07d0645d534a52142b28
daDevBoat <elias.richard.naess@gmail.com>
2025-11-14 21:04:15 +0100

    All tables (for now) added totaling 13

--- DB/init.sql
+++ DB/init.sql
@@ -6,6 +6,11 @@ CREATE DATABASE university_db;
 DROP TYPE IF EXISTS period_enum;
 CREATE TYPE period_enum AS ENUM('1', '2', '3', '4');
 
+CREATE TABLE rules (
+    name VARCHAR(50) PRIMARY KEY,
+    limit_value INT NOT NULL
+);
+
 CREATE TABLE course_layout (
     course_layout_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     course_code VARCHAR(10) NOT NULL,
@@ -58,4 +63,34 @@ CREATE TABLE skill_set (
     skill VARCHAR(100) NOT NULL UNIQUE
 );
 
+CREATE TABLE employee (
+    employement_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    person_id INT REFERENCES person(person_id) NOT NULL,
+    department_id INT REFERENCES department(department_id) NOT NULL,
+    job_title_id INT REFERENCES job_title(job_title_id) NOT NULL,
+    supervisor_id INT REFERENCES employee(employement_id),
+    is_active BIT(1) DEFAULT B'1' NOT NULL
+);
+
+CREATE TABLE salary_history (
+    employement_id INT REFERENCES employee(employement_id) NOT NULL,
+    year INT NOT NULL,
+    period period_enum NOT NULL,
+    PRIMARY KEY (employement_id, year, period)
+);
+
+CREATE TABLE employee_skill_set (
+    employement_id INT REFERENCES employee(employement_id) NOT NULL,
+    skill_set_id INT REFERENCES skill_set(skill_set_id) NOT NULL,
+    PRIMARY KEY (employement_id, skill_set_id)
+);
+
+CREATE TABLE employee_planned_activity (
+    employement_id INT REFERENCES employee(employement_id) NOT NULL,
+    planned_activity_id INT REFERENCES planned_activity(planned_activity_id) NOT NULL,
+    PRIMARY KEY (employement_id, planned_activity_id)
+);
+
+
+/* TRIGGERS and FUNCTIONS */
 
